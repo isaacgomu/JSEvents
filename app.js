3343fe146d015be30199546c1fb65ad14e9bb3d1
isaacgomu <IsaacDalglish03@gmail.com>
2024-03-21 17:00:26 +0000

    added a header and paragraph which change colour depending on whether in light mode or dark

--- app.js
+++ app.js
@@ -12,8 +12,12 @@ buttonhover.addEventListener("mouseleave", function () {
   console.log("put me back!");
 });
 
+const h1 = document.querySelector("h1");
+const p = document.querySelector("p");
 const lightlevel = document.getElementById("lightlevel");
 let light = 0;
+h1.style.color = "#000141";
+p.style.color = "#000141";
 lightlevel.addEventListener("click", function () {
   if (light === 0) {
     light += 1;
@@ -22,7 +26,11 @@ lightlevel.addEventListener("click", function () {
   }
   if (light === 0) {
     document.body.style.backgroundColor = "#ffffff";
+    h1.style.color = "#000141";
+    p.style.color = "#000141";
   } else if (light === 1) {
     document.body.style.backgroundColor = "#000000";
+    h1.style.color = "#2f0085";
+    p.style.color = "#4b01d5";
   }
 });
